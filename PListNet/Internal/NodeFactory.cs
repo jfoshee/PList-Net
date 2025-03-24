d8fdb5405a76ec40fb82fcc91456f0908e492649
Jacob Foshee <jacobf@gmail.com>
2025-03-24 08:54:09 -0500

    fix: add lock to NodeFactory so that factory methods cannot be called while the factory is being reset or registrations are being made
    this resolves the failing test from the parent commit

--- PListNet/Internal/NodeFactory.cs
+++ PListNet/Internal/NodeFactory.cs
@@ -7,120 +7,99 @@ namespace PListNet.Internal;
 /// </summary>
 public static class NodeFactory
 {
-	private static readonly Dictionary<string, Type> _xmlTags = new Dictionary<string, Type>();
-	private static readonly Dictionary<byte, Type> _binaryTags = new Dictionary<byte, Type>();
+	private static readonly Dictionary<string, Type> _xmlTags = new();
+	private static readonly Dictionary<byte, Type> _binaryTags = new();
+	private static readonly object _lock = new();
 
 	/// <summary>
 	/// Initializes the <see cref="NodeFactory"/> class.
 	/// </summary>
 	static NodeFactory()
-    {
-        Reset();
-    }
+	{
+		Reset();
+	}
 
 	/// <summary>
 	/// Resets the factory to default tags.
 	/// </summary>
 	internal static void Reset()
 	{
-		_xmlTags.Clear();
-		_binaryTags.Clear();
-		Register(new DictionaryNode());
-		Register(new IntegerNode());
-		Register(new RealNode());
-		Register(new StringNode());
-		Register(new ArrayNode());
-		Register(new DataNode());
-		Register(new DateNode());
-		Register(new UidNode());
+		lock (_lock)
+		{
+			_xmlTags.Clear();
+			_binaryTags.Clear();
+			RegisterInternal(new DictionaryNode());
+			RegisterInternal(new IntegerNode());
+			RegisterInternal(new RealNode());
+			RegisterInternal(new StringNode());
+			RegisterInternal(new ArrayNode());
+			RegisterInternal(new DataNode());
+			RegisterInternal(new DateNode());
+			RegisterInternal(new UidNode());
 
-		Register("string", 5, new StringNode());
-		Register("ustring", 6, new StringNode());
+			RegisterInternal("string", 5, new StringNode());
+			RegisterInternal("ustring", 6, new StringNode());
 
-		Register("true", 0, new BooleanNode());
-		Register("false", 0, new BooleanNode());
+			RegisterInternal("true", 0, new BooleanNode());
+			RegisterInternal("false", 0, new BooleanNode());
+		}
 	}
 
-    /// <summary>
-    /// Registers the specified element type by its own XmlTag and BinaryTag.
-    /// If the tag already exists, it will be overwritten.
-    /// </summary>
-    /// <typeparam name="T">The node type to register.</typeparam>
-    /// <param name="node">An instance of the node used to extract tags.</param>
-    public static void Register<T>(T node) where T : PNode, new()
+	public static void Register<T>(T node) where T : PNode, new()
 	{
-		Register(node.XmlTag, node.BinaryTag, node);
+		lock (_lock)
+		{
+			RegisterInternal(node.XmlTag, node.BinaryTag, node);
+		}
 	}
 
-	/// <summary>
-	/// Registers the specified element type with custom XML and binary tags.
-	/// If the tag already exists, it will be overwritten.
-	/// </summary>
-	/// <typeparam name="T">The node type to register.</typeparam>
-	/// <param name="xmlTag">The XML tag to associate with the node type.</param>
-	/// <param name="binaryTag">The binary tag to associate with the node type.</param>
-	/// <param name="node">An instance of the node to get the type from.</param>
 	public static void Register<T>(string xmlTag, byte binaryTag, T node) where T : PNode, new()
+	{
+		lock (_lock)
+		{
+			RegisterInternal(xmlTag, binaryTag, node);
+		}
+	}
+
+	private static void RegisterInternal<T>(T node) where T : PNode, new()
+		=> RegisterInternal(node.XmlTag, node.BinaryTag, node);
+
+	private static void RegisterInternal<T>(string xmlTag, byte binaryTag, T node) where T : PNode, new()
 	{
 		_xmlTags[xmlTag] = node.GetType();
 		_binaryTags[binaryTag] = node.GetType();
 	}
 
-	/// <summary>
-	/// Creates a concrete <see cref="T:PListNet.PNode"/> object secified specified by it's typecode.
-	/// </summary>
-	/// <param name="binaryTag">The typecode of the element.</param>
-	/// <param name="length">The length of the element 
-	/// (required only for <see cref="T:PListNet.Primitives.PListBool"/>, <see cref="T:PListNet.Primitives.PListNull"/>
-	/// and <see cref="T:PListNet.Primitives.PListFill"/>).</param>
-	/// <returns>The created <see cref="T:PListNet.PNode"/> object</returns>
 	public static PNode Create(byte binaryTag, int length)
 	{
-		if (binaryTag == 0 && length == 0x00) return new NullNode();
-		if (binaryTag == 0 && length == 0x0F) return new FillNode();
-
-		if (binaryTag == 6) return new StringNode { IsUtf16 = true };
-
-		if (_binaryTags.ContainsKey(binaryTag))
+		lock (_lock)
 		{
-			return (PNode)Activator.CreateInstance(_binaryTags[binaryTag]);
-		}
+			if (binaryTag == 0 && length == 0x00) return new NullNode();
+			if (binaryTag == 0 && length == 0x0F) return new FillNode();
 
-		throw new PListFormatException($"Unknown node - binary tag {binaryTag}");
+			if (binaryTag == 6) return new StringNode { IsUtf16 = true };
+
+			if (_binaryTags.TryGetValue(binaryTag, out var type))
+				return (PNode)Activator.CreateInstance(type)!;
+
+			throw new PListFormatException($"Unknown node - binary tag {binaryTag}");
+		}
 	}
 
-	/// <summary>        
-	/// Creates a concrete <see cref="T:PListNet.PNode"/> object secified specified by it's tag.
-	/// </summary>
-	/// <param name="tag">The tag of the element.</param>
-	/// <returns>The created <see cref="T:PListNet.PNode"/> object</returns>
 	public static PNode Create(string tag)
 	{
-		if (_xmlTags.ContainsKey(tag))
+		lock (_lock)
 		{
-			return (PNode)Activator.CreateInstance(_xmlTags[tag]);
+			if (_xmlTags.TryGetValue(tag, out var type))
+				return (PNode)Activator.CreateInstance(type)!;
+
+			throw new PListFormatException($"Unknown node - XML tag \"{tag}\"");
 		}
-
-		throw new PListFormatException($"Unknown node - XML tag \"{tag}\"");
 	}
 
-	/// <summary>
-	/// Creates a <see cref="T:PListNet.PNode"/> object used for exteded length information.
-	/// </summary>
-	/// <param name="length">The exteded length information.</param>
-	/// <returns>The <see cref="T:PListNet.PNode"/> object used for exteded length information.</returns>
 	public static PNode CreateLengthElement(int length)
-	{
-		return new IntegerNode(length);
-	}
+		=> new IntegerNode(length);
 
-	/// <summary>
-	/// Creates a <see cref="T:PListNet.PNode"/> object used for dictionary keys.
-	/// </summary>
-	/// <param name="key">The key.</param>
-	/// <returns>The <see cref="T:PListNet.PNode"/> object used for dictionary keys.</returns>
 	public static PNode CreateKeyElement(string key)
-	{
-		return new StringNode(key);
-	}
+		=> new StringNode(key);
 }
